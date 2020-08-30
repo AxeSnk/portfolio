626734da30c538ddd78d6c6ecdd1c4f730b622fa
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-08-30 12:20:45 +0800

    Updates

---
+++ precache-manifest.0ea40d5bbd159a6a0f20fe7d909f24a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c5e46708ee00cb944e1c59db249074",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "18c0d06b5a84f2f8138e",
+    "url": "/portfolio/static/css/main.d3a3dccf.chunk.css"
+  },
+  {
+    "revision": "599045ab523c9c9a0999",
+    "url": "/portfolio/static/js/2.8b295149.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8b295149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c0d06b5a84f2f8138e",
+    "url": "/portfolio/static/js/main.e02e4755.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f1fd53eb257f178080435a16480d1b46",
+    "url": "/portfolio/static/media/home.f1fd53eb.jpg"
+  },
+  {
+    "revision": "259115c502963b9db03fc28dafebef90",
+    "url": "/portfolio/static/media/nothing-you-could-do.259115c5.ttf"
+  },
+  {
+    "revision": "62b71f2ec6aa64612a6e5c9aa9fcc444",
+    "url": "/portfolio/static/media/nothing-you-could-do.62b71f2e.eot"
+  },
+  {
+    "revision": "69e191adda074a99c915a68dc73f5dcd",
+    "url": "/portfolio/static/media/nothing-you-could-do.69e191ad.woff"
+  },
+  {
+    "revision": "4312f1fbdcf4d54af4506dabdce08010",
+    "url": "/portfolio/static/media/roboto.4312f1fb.ttf"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/portfolio/static/media/roboto.94dac78e.woff"
+  },
+  {
+    "revision": "b9077621ce786b55c176a61456bfc077",
+    "url": "/portfolio/static/media/roboto.b9077621.eot"
+  }
+]);
